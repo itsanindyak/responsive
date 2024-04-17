a7e18a98a46952297aae70973f8b3f44356735e3
itsanindyak <iankoley04@gmail.com>
2024-04-17 23:22:02 +0530

    add responsive

---
+++ app.js
@@ -0,0 +1,11 @@
+let navLink = document.getElementById("nav-links");
+let toggle = document.getElementById("toggle");
+
+
+
+toggle.addEventListener('click',()=>{
+
+    navLink.classList.toggle("active");
+
+
+})
